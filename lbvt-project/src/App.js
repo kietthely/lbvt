bbca4ac3af0431201676754e5d6f18fc0692de25
Sammie <lythekiet333@gmail.com>
2024-03-25 17:01:05 +1030

    Add events to year 1 building
    if year 1 building is clicked then print out a message to tell that this building is being clicked

--- lbvt-project/src/App.js
+++ lbvt-project/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import Spline from "@splinetool/react-spline";
-import db from "./firebase.js"; 
+import db from "./firebase.js";
 // to pass, get info to firebase
 import { collection, addDoc, getDocs } from "firebase/firestore";
-import {useState, useEffect} from "react";
+import React, { useState, useEffect, Suspense } from "react";
+import Spline from "@splinetool/react-spline";
 
 export default function App() {
-
+  /*
   // Fetch info to get Program info.
   // Get all info about the program(LBVT)
   const [programs, setProgram] = useState([]);
@@ -19,15 +19,32 @@ export default function App() {
     getDocs(programData).then((snapshot) => {
       // set information to the programs
       //console.log(snapshot.docs.map((doc) => ({...doc.data()})));
-      setProgram(snapshot.docs.map((doc) => ({...doc.data()})));
+      setProgram(snapshot.docs.map((doc) => ({ ...doc.data() })));
       setCourses(programs[0].Courses);
     });
   }, []);
 
   console.log(courses);
-  
+*/
+  // spline section
+  function onMouseDown(e) {
+    if (e.target.name === "year1_building_1") {
+      console.log("year 1 building have been clicked!");
+    } else if (e.target.name === "year1_building_2") {
+      console.log("year 1 building 2 have been clicked!");
+    } else if (e.target.name === "year1_building_3") {
+      console.log("year 1 building 3 have been clicked!");
+    } else if (e.target.name === "year1_building_4") {
+      console.log("year 1 building 4 have been clicked!");
+    }
+  }
   return (
     // need to find a way how to set the info to the each building
-    <Spline scene="https://prod.spline.design/RUIIGY18trV33sg0/scene.splinecode" />
+    <div id="canvas3d">
+      <Spline
+        scene="https://prod.spline.design/RUIIGY18trV33sg0/scene.splinecode"
+        onMouseDown={onMouseDown}
+      />
+    </div>
   );
 }
