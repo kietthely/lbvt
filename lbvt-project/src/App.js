ec7c2e771dac7e28b2f4fde784819644f8590228
YutaKasuga <127809943+YutaKasuga@users.noreply.github.com>
2024-03-26 13:22:44 +1030

    Update App.js
    Added functions that will control events after the loading

--- lbvt-project/src/App.js
+++ lbvt-project/src/App.js
@@ -47,7 +47,47 @@ export default function App() {
     });
   }, []);
 
-  console.log(courses);
+  function onLoad(Spline){
+    /* Connect variable and object in Spline from Spline's objId*/
+
+    
+    const test = Spline.findObjectByName("year1_lbvt_building_4");
+    console.log(test);
+
+    /*
+    // Year 1 (sp2)
+    const obj_BIOL1014 = Spline.findObjectByName("year1_lbvt_building_4")
+    const obj_BIOL1015 = Spline.findObjectById();
+    const obj_EART1010 = Spline.findObjectById();
+    const obj_EART1011 = Spline.findObjectById();
+    // Year 1 (sp5)
+    const obj_ENVT1013 = Spline.findObjectById();
+    const obj_ENVT1016 = Spline.findObjectById();
+    const obj_GEOE1018 = Spline.findObjectById();
+    const obj_GEOE2026 = Spline.findObjectById();
+
+    // Year 2 (sp2)
+    const obj_BIOL2023 = Spline.findObjectById();
+    const obj_EART3012 = Spline.findObjectById();
+    const obj_ENVT1019 = Spline.findObjectById();
+    const obj_ENVT3016 = Spline.findObjectById();
+    // Year 2 (sp5)
+    const obj_EE1 = Spline.findObjectById();
+    const obj_EE2 = Spline.findObjectById();
+    const obj_GEOE2022= Spline.findObjectById();
+    const obj_GEOE3019= Spline.findObjectById();
+
+    // Year 3 (sp2)
+    const obj_ENVT3030 = Spline.findObjectById();
+    const obj_ENVT3031 = Spline.findObjectById();
+    const obj_EE3 = Spline.findObjectById();
+    const obj_EE4 = Spline.findObjectById();
+    // Year 3 (sp5)
+    const obj_EE5 = Spline.findObjectById();
+    const obj_EE6 = Spline.findObjectById();
+    const obj_UE1= Spline.findObjectById();
+    */
+  }
   
   return (
     // need to find a way how to set the info to the each building
