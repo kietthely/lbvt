ceeac49458fe5287705b5375e8d4caf1b4f110f8
Sammie <lythekiet333@gmail.com>
2024-04-09 15:03:04 +0930

    Import tailwind.css
    Import tailwind.css

---
+++ lbvt-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+const App = () => {
+  <main className="bg-slate-300/20">
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/help" element={"Help"} />
+      </Routes>
+    </Router>
+  </main>;
+};
+export default App;
