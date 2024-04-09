288681085a8fc38e22f6840d14c6175af0ed5aa8
Sammie <lythekiet333@gmail.com>
2024-04-09 15:39:20 +0930

    Change project folder to vite.js
    Change development environment

---
+++ lbvt-roadmap/src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-20 h-20"></div>
+    </div>
+  );
+};
+
+export default Loader;
