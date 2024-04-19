e863dd26d58d15dd047a52b213b082a37cf247a7
Sammie <lythekiet333@gmail.com>
2024-04-20 03:19:33 +0930

    Load the scene
    Sets up the scene and the camera

--- lbvt-roadmap/src/components/Home.jsx
+++ lbvt-roadmap/src/components/Home.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import * as Three from "three";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 const Home = () => {
-  return (
-    <section className="w-full h-screen relative">
-      <div
-        className="absolute top-28 left-0 right-0 z-10 flex 
-        items-center justify-center"
-      >
-        Popup
-      </div>
-    </section>
+  var scene, camera, renderer, controls;
+  scene = new Three.Scene();
+  camera = new Three.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
   );
+  renderer = new Three.WebGLRenderer();
+
+  const loader = new GLTFLoader();
+  loader.load("../assets/lbvt.glb", function (gltf) {});
+
+  return <div></div>;
 };
 
 export default Home;
