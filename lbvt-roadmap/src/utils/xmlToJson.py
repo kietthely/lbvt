354f3ec1e8f5eebce2d66c6603726b2c41d0b4ab
Sammie <lythekiet333@gmail.com>
2024-04-19 18:11:57 +0930

    Re-generate json file
    Update json file accordingly with the xml file.
    Added industries and assistances.

--- lbvt-roadmap/src/utils/xmlToJson.py
+++ lbvt-roadmap/src/utils/xmlToJson.py
@@ -2,16 +2,20 @@ import xmltodict
 import json
 
 def convert_xml_to_json(xml_file):
+    # Read xml file
     with open(xml_file, 'r') as file:
         xml_content = file.read()
-    data_dict = xmltodict.parse(xml_content)
+    # Parse xml content
+    # Some elements have only 1 child but they are supposed to be a list
+    data_dict = xmltodict.parse(xml_content, force_list={'courseCoordinator','prerequisite'})
     json_data = json.dumps(data_dict)
     return json_data
-
+# Put your xml file path here
 json_data = convert_xml_to_json(r'C:\Users\lythe\Downloads\my_projects\react-practice\lbvt\lbvt-roadmap\src\assets\lbvt.xml')
 
 def write_json_to_file(json_data, file_path):
+    # Generate a json file
     with open(file_path, 'w') as file:
         file.write(json_data)
-
+# Put your json file path here 
 write_json_to_file(json_data, r'C:\Users\lythe\Downloads\my_projects\react-practice\lbvt\lbvt-roadmap\src\assets\lbvt.json')
