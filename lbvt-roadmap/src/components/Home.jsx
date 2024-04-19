ba2b9baa55816446ed659d1377b7d14915af46d5
Sammie <lythekiet333@gmail.com>
2024-04-20 03:09:39 +0930

    Update handling sp num
    Dynamically handle sp numb.
    Re-export the model with the script created

--- lbvt-roadmap/src/components/Home.jsx
+++ lbvt-roadmap/src/components/Home.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import { Suspense } from "react";
-import { Canvas } from "react-three-fiber";
-import Loader from "./Loader";
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
@@ -11,18 +8,6 @@ const Home = () => {
       >
         Popup
       </div>
-      <Canvas
-        className="w-full h-screen bg-transparent "
-        camera={{ near: 0.1, far: 1000 }}
-      >
-        <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
-        </Suspense>
-      </Canvas>
     </section>
   );
 };
