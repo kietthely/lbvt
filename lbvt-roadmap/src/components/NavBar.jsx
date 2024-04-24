2a528a1f10fa4f4c843ba604a80f6092871903af
Sammie <lythekiet333@gmail.com>
2024-04-24 10:44:39 +0930

    Revert "Update design"
    This reverts commit 02771df9a03bc53dbb990ba406f2d655bd13b562.

--- lbvt-roadmap/src/components/NavBar.jsx
+++ lbvt-roadmap/src/components/NavBar.jsx
@@ -6,9 +6,9 @@ const NavBar = () => {
     <header className="header">
       <NavLink
         to="/"
-        className="w-14 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
       >
-        <p className="blue-gradient_text">LBVT</p>
+        <p className="blue-gradient_text">AH</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
         <NavLink
