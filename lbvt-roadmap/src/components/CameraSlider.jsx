866e3d0b4b56c7312a3fd71941c566d0142782f7
SammieDev <lythekiet333@gmail.com>
2024-05-04 22:20:48 +0930

    Add icons for the slider

--- lbvt-roadmap/src/components/CameraSlider.jsx
+++ lbvt-roadmap/src/components/CameraSlider.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 function ControlButtons({ camera }) {
   const [fov, setFov] = useState(camera.fov);
 
@@ -12,15 +14,39 @@ function ControlButtons({ camera }) {
     }
   };
 
+  const handleZoomIn = () => {
+    const newFov = Math.max(10, fov - 1);
+    setFov(newFov);
+    if (camera) {
+      camera.fov = newFov;
+      camera.updateProjectionMatrix();
+    }
+  };
+
+  const handleZoomOut = () => {
+    const newFov = Math.min(100, fov + 1);
+    setFov(newFov);
+    if (camera) {
+      camera.fov = newFov;
+      camera.updateProjectionMatrix();
+    }
+  };
+
   return (
-    <div className="control-buttons">
+    <div>
+      <Button onClick={handleZoomOut}>
+        <FontAwesomeIcon icon={faMinus} />
+      </Button>
       <input
         type="range"
-        min="1"
-        max="75"
+        min="10"
+        max="100"
         value={fov}
         onChange={handleFovChange}
       />
+      <Button onClick={handleZoomIn}>
+        <FontAwesomeIcon icon={faPlus} />
+      </Button>
     </div>
   );
 }
