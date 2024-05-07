a800775212db5a84c7b01fab625b5abf6471c0e0
YutaKasuga <127809943+YutaKasuga@users.noreply.github.com>
2024-05-08 03:27:13 +0930

    Modified the displayCourseUI() to use json data
    modified the parameter that came from 3d object to json for future's modification to allow importing other program's data.

--- lbvt-roadmap/src/components/Home.jsx
+++ lbvt-roadmap/src/components/Home.jsx
@@ -10,13 +10,13 @@ import CameraSlider from "./CameraSlider";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass.js";
 import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
-import lbvt_data from "../assets/lbvt.json";
+//import lbvt_data from "../assets/lbvt.json";
 const Home = () => {
   const ref = useRef();
   const [camera, setCamera] = useState(null);
   const controls = useRef();
   // lbvt data
-  const courses_data = lbvt_data.repository.program.courses;
+  //const courses_data = lbvt_data.repository.program.courses;
   useEffect(() => {
     // initialize world
     var scene, cam, renderer;
@@ -106,152 +106,162 @@ const Home = () => {
         outlinePass.selectedObjects = [selectedObject];
       }
       // course data
+      const courses_data = data.repository.program.courses;
 
       // if year1_sp2_building_1, year1_sp2_building_2, year1_sp2_building_3, year1_sp2_building_4
       switch (intersects[0].object.parent.name) {
         // for year1 sp2
-        case "year1_sp2_building_1":
-          console.log("year1_sp2_building_1 clicked");
-          let sp2_course_1 = courses_data.year1.sp2.course[0];
-          console.log(sp2_course_1);
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp2_building_1": {
+          const y1_sp2_course_1 = courses_data.year1.sp2.course[0];
+          displayCourseUI(y1_sp2_course_1);
+          }
           break;
-        case "year1_sp2_building_2":
-          console.log("year1_sp2_building_2 clicked");
-          console.log(courses_data[1]);
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp2_building_2":{
+          const y1_sp2_course_2 = courses_data.year1.sp2.course[1];
+          displayCourseUI(y1_sp2_course_2);
+          }
           break;
-        case "year1_sp2_building_3":
-          console.log("year1_sp2_building_3 clicked");
-          console.log(courses_data[2]);
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp2_building_3": {
+          const y1_sp2_course_3 = courses_data.year1.sp2.course[2];
+          displayCourseUI(y1_sp2_course_3);
+          }
           break;
-        case "year1_sp2_building_4":
-          console.log("year1_sp2_building_4 clicked");
-          console.log(courses_data[3]);
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp2_building_4": {
+          const y1_sp2_course_4 = courses_data.year1.sp2.course[3];
+          displayCourseUI(y1_sp2_course_4);
+          }
           break;
 
         // for year1 sp5
-        case "year1_sp5_building_1":
-          console.log("year1_sp5_building_1 clicked");
-          console.log(courses_data);
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp5_building_1": {
+          const y1_sp5_course_1 = courses_data.year1.sp5.course[0];
+          displayCourseUI(y1_sp5_course_1);
+          }
           break;
-        case "year1_sp5_building_2":
-          console.log("year1_sp5_building_2 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp5_building_2": {
+          const y1_sp5_course_2 = courses_data.year1.sp5.course[1];
+          displayCourseUI(y1_sp5_course_2);
+          }
           break;
-        case "year1_sp5_building_3":
-          console.log("year1_sp5_building_3 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp5_building_3": {
+          const y1_sp5_course_3 = courses_data.year1.sp5.course[2];
+          displayCourseUI(y1_sp5_course_3);
+          }
           break;
-        case "year1_sp5_building_4":
-          console.log("year1_sp5_building_4 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year1_sp5_building_4": {
+          const y1_sp5_course_4 = courses_data.year1.sp5.course[3];
+          displayCourseUI(y1_sp5_course_4);
+          }
           break;
 
         // for year2 sp2
-        case "year2_sp2_building_1":
-          console.log("year2_sp2_building_1 clicked");
+        case "year2_sp2_building_1": {
+          const y2_sp2_course_1 = courses_data.year2.sp2.course[0];
+          displayCourseUI(y2_sp2_course_1);
+          }
 
-          displayCourseUI(intersects[0].object.parent.userData);
           break;
-        case "year2_sp2_building_2":
-          console.log("year2_sp2_building_2 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year2_sp2_building_2": {
+          const y2_sp2_course_2 = courses_data.year2.sp2.course[1];
+          displayCourseUI(y2_sp2_course_2);
+          }
           break;
-        case "year2_sp2_building_3":
-          console.log("year2_sp2_building_3 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year2_sp2_building_3": {
+          const y2_sp2_course_3 = courses_data.year2.sp2.course[2];
+          displayCourseUI(y2_sp2_course_3);
+          }
           break;
-        case "year2_sp2_building_4":
-          console.log("year2_sp2_building_4 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year2_sp2_building_4": {
+          const y2_sp2_course_4 = courses_data.year2.sp2.course[3];
+          displayCourseUI(y2_sp2_course_4);
+          }
           break;
 
         // for year2 sp5
-        case "year2_sp5_building_1":
-          console.log("year2_sp5_building_1 clicked");
-
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year2_sp5_building_1": {
+          const y2_sp5_course_1 = courses_data.year2.sp5.course[0];
+          displayCourseUI(y2_sp5_course_1);
+          }
           break;
-        case "year2_sp5_building_2":
-          console.log("year2_sp5_building_2 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year2_sp5_building_2": {
+          const y2_sp5_course_2 = courses_data.year2.sp5.course[1];
+          displayCourseUI(y2_sp5_course_2);
+          }
           break;
-        case "year2_sp5_building_3":
-          console.log("year2_sp5_building_3 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year2_sp5_building_3": {
+          const y2_sp5_course_3 = courses_data.year2.sp5.course[2];
+          displayCourseUI(y2_sp5_course_3);
+          }
           break;
-        case "year2_sp5_building_4":
-          console.log("year2_sp5_building_4 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year2_sp5_building_4": {
+          const y2_sp5_course_4 = courses_data.year2.sp5.course[3];
+          displayCourseUI(y2_sp5_course_4);
+          }
           break;
 
         // for year3 sp2
-        case "year3_sp2_building_1":
-          console.log("year3_sp2_building_1 clicked");
-          console.log(intersects[0].object.parent.userData);
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year3_sp2_building_1": {
+          const y3_sp2_course_1 = courses_data.year3.sp2.course[0];
+          displayCourseUI(y3_sp2_course_1);
+          }
           break;
-        case "year3_sp2_building_2":
-          console.log("year3_sp2_building_2 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year3_sp2_building_2": {
+          const y3_sp2_course_2 = courses_data.year3.sp2.course[1];
+          displayCourseUI(y3_sp2_course_2);
+          }
           break;
-        case "year3_sp2_building_3":
-          console.log("year3_sp2_building_3 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year3_sp2_building_3": {
+          const y3_sp2_course_3 = courses_data.year3.sp2.course[2];
+          displayCourseUI(y3_sp2_course_3);
+          }
           break;
-        case "year3_sp2_building_4":
-          console.log("year3_sp2_building_4 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+        case "year3_sp2_building_4": {
+          const y3_sp2_course_4 = courses_data.year3.sp2.course[3];
+          displayCourseUI(y3_sp2_course_4);
+          }
           break;
 
         // for year3 sp5
-        case "year3_sp5_building_1":
+        case "year3_sp5_building_1": {
           console.log("year3_sp5_building_1 clicked");
-          console.log(intersects[0].object.parent.userData);
-          displayCourseUI(intersects[0].object.parent.userData);
+          const y3_sp5_course_1 = courses_data.year3.sp5.course[0];
+          displayCourseUI(y3_sp5_course_1);
+          }
           break;
-        case "year3_sp5_building_2":
+        case "year3_sp5_building_2": {
           console.log("year3_sp5_building_2 clicked");
-          console.log(intersects[0].object.parent.userData);
-          displayCourseUI(intersects[0].object.parent.userData);
+          const y3_sp5_course_2 = courses_data.year3.sp5.course[1];
+          displayCourseUI(y3_sp5_course_2);
+          }
           break;
-        case "year3_sp5_building_3":
+        case "year3_sp5_building_3": {
           console.log("year3_sp5_building_3 clicked");
-          displayCourseUI(intersects[0].object.parent.userData);
+          const y3_sp5_course_3 = courses_data.year3.sp5.course[2];
+          displayCourseUI(y3_sp5_course_3);
+          }
           break;
 
         // for Elective
         case "elective_lbvt":
-          console.log("elective clicked");
           displayElectiveUI();
           break;
 
         // for Alumni
         case "alumni_lbvt":
-          console.log("alumni clicked");
           //<Alumni/>
           displayAlumniUI();
           break;
 
         // for Industry
         case "industry_lbvt":
-          console.log("industry clicked");
           displayIndustryUI();
           break;
 
         // for welcome building
         case "great_hall":
-          console.log("great hall clicked");
           displayWelcomeUI();
           break;
-        // for great hall
-        case "great_hall":
-          console.log("great hall clicked");
-          break;
+
         default:
           console.log(intersects[0].object);
           break;
@@ -298,103 +308,55 @@ const Home = () => {
       controls.current.reset();
     }
   };
-  function displayCourseUI(evt) {
+function displayCourseUI(course) {
     // display UI for course information with the related event
-    // parameter: -> userData (course information from click event)
-    if (evt.course_id != null) {
-      // check the event has by the specific event (for the course check course id)
-      var courseUI = window.open("", "_blank", "width=600, height=400"); // <- should I change here to the window.alert()?
+    // parameter: -> course information as an object type from click event with course building.
+
+    if (course.id != null) {
+      // check the parameter has id (for checking course id)
+      var courseUI = window.open("", "_blank", "width=600, height=400"); 
       courseUI.document.write("<div id ='courseUI'>");
 
-      courseUI.document.write(
-        "<p>" + "Course Name: " + evt.course_name + "</p>"
-      );
-      courseUI.document.write("<p>" + "Course ID: " + evt.course_id + "</p>");
+      courseUI.document.write("<h3>" + course.name + "</h3>");
+      courseUI.document.write("<p>Course ID: " + course.id + "</p>");
 
-      if (evt.course_coordinator_size != 0) {
-        // display all course coordinator's information
-
-        // To detect the pattern in the userData.
-        const patternForName = /^course_coordinator_\d+_name$/;
-        const patternForUrl = /^course_coordinator_\d+_url$/;
-        for (let key in evt) {
-          if (patternForName.test(key)) {
-            const keyValueName = key.match(/\d+/)[0]; // detect the amount of coordinators in userData
-            //console.log(`Course Coordinator: ${evt[key]}`)
-            if (evt.course_coordinator_size > 1) {
-              // if the course has several coordinators display index value
-              courseUI.document.write(
-                "<p>" +
-                  "Course coordinator " +
-                  keyValueName +
-                  ": " +
-                  evt[key] +
-                  "<br>"
-              );
-            } else {
-              courseUI.document.write(
-                "<p>" + "Course coordinator: " + evt[key] + "<br>"
-              );
-            }
-          }
-          if (patternForUrl.test(key)) {
-            //const keyValueUrl = key.match(/\d+/)[0]; // detect the amount of coordinator's url in userData
-            courseUI.document.write(
-              "More information: " +
-                "<a href=" +
-                evt[key] +
-                ' target="_blank" rel="noopener noreferrer">Link</a></p>'
-            );
-          }
-        }
-      }
-      if (evt.prerequisite_size != 1) {
-        // if the course has prerequisites, display all id of the course
-        // In Sam's code, it seems if there is NO prerequisite, the number will be 1.
-
-        const patternForPrerequisite = /^prerequisite_\d+_id$/; // to detect the prerequisite pattern in for loop
-
-        for (let key in evt) {
-          if (patternForPrerequisite.test(key)) {
-            const keyValueId = key.match(/\d+/)[0]; // detect the amount of prerequisite in userData
-            if (evt.prerequisite_size > 2) {
-              // if the course has several prerequisites display index value
-              courseUI.document.write(
-                "<p>" + "Prerequisite " + keyValueId + ": " + evt[key] + "</p>"
-              );
-            } else {
-              courseUI.document.write(
-                "<p>" + "Prerequisite : " + evt[key] + "</p>"
-              );
-            }
-          }
+      if (course.courseCoordinators.courseCoordinator.length != 1) {
+        // Check the course has multiple coordinators
+        for (let i = 0; i < course.courseCoordinators.courseCoordinator.length; i++) {
+          // display all course coordinator's information
+          courseUI.document.write("<p>Course coordinator: <a href="+course.courseCoordinators.courseCoordinator[i].url +' target="_blank" rel="noopener noreferrer">'+course.courseCoordinators.courseCoordinator[i].name +"</a></p>");
         }
       } else {
-        courseUI.document.write("<p>" + "Prerequisite : N/A </p>");
+        // case for there is only 1 course coordinator
+        if (course.courseCoordinators.courseCoordinator[0].name != null){
+          courseUI.document.write("<p>Course coordinator: <a href="+course.courseCoordinators.courseCoordinator[0].url +' target="_blank" rel="noopener noreferrer">'+course.courseCoordinators.courseCoordinator[0].name +"</a></p>");
+        } else {
+          // case for the course does not have course coordinator (ex. elective courses)
+          courseUI.document.write("<p>Course coordinator: N/A</p>");
+        }
+      }
+        
+      for (let i = 0; i < course.prerequisites.prerequisite.length; i++) {
+        // display all prerequisite information
+        if (course.prerequisites.prerequisite[i].id != null){
+          courseUI.document.write("<p>Prerequisite: " +course.prerequisites.prerequisite[i].id +"</p>");
+        } else {
+          // case for the course does not have prerequisite
+          courseUI.document.write("<p>Prerequisite: N/A</p>");
+        }
       }
 
-      courseUI.document.write(
-        "<p>" +
-          "Course web page: " +
-          "<a href=" +
-          evt.course_url +
-          ' target="_blank" rel="noopener noreferrer">Link</a></p>'
-      );
-
-      if ("notes" in evt) {
-        // if the course has any note, display the information
-        //for (let i = 0; i < evt.notes_size; i++ ){
-        courseUI.document.write("<p>" + "Note: " + evt.notes + "</p>");
-        //}
+      if (course.notes.note != null){
+        // check the course has specific information as note.
+          courseUI.document.write("<p>Note: " +course.notes.note +"</p>");
       }
 
-      if ("rules" in evt) {
-        // if the course has any rules, display the information
-        //for (let i = 0; i < evt.rules_size; i ++){
-        courseUI.document.write("<p>" + "Rule: " + evt.rules + "</p>");
-        //}
+      if (course.rules.rule != null){
+        // check the course has specific information as rule.
+          courseUI.document.write("<p>Rule: " +course.rules.rule +"</p>");
       }
-      courseUI.document.write("</div>");
+      // display the link for the Uni SA's course page
+      courseUI.document.write("<p>More information about the course: <a href="+course.url +' target="_blank" rel="noopener noreferrer">Link</a></p>');
     }
   }
   function displayElectiveUI(){
