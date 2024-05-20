0ada4f898a1d16a3d38c936e994d7657ea3bcebd
YutaKasuga <127809943+YutaKasuga@users.noreply.github.com>
2024-05-20 16:40:43 +0930

    Fixed error
    Due to having error for function() in return statement, modified the section and put setAlumniInfo() instead of previous solution.

--- lbvt-roadmap/src/components/Alumni.jsx
+++ lbvt-roadmap/src/components/Alumni.jsx
@@ -1,46 +1,33 @@
-//import React, { useEffect, useRef, useState } from "react";
+import { Link } from 'react-router-dom';
 import data from '../assets/lbvt.json';
 
 const Alumni = () => {
     const alumniData = data.repository.alumnus;
-    /*
-    function displayAlumniUI() {
-        // display UI for Alumni information with the related event
-        // source data is came from "src\assets\lbvt.json"
-        const alumniData = data.repository.alumnus;
-        console.log(data.repository);
-        var courseUI = window.open("", "_blank", "width=600, height=400"); // need to consider to change design
-        courseUI.document.write("<div id ='alumniUI'>");
-    
-        courseUI.document.write("<h2>Alumni</h2>");
-        if ("alumni" in alumniData) {
-          for (let i = 0; i < alumniData.alumni.length; i++) {
-            courseUI.document.write("<p>" + alumniData.alumni[i].name + "<br/>");
-            courseUI.document.write(
-              "<a href=" +
-                alumniData.alumni[i].url +
-                ' target="_blank" rel="noopener noreferrer">Link</a></p>'
-            );
-          }
-        } else {
-          courseUI.document.write("<p>Oops, There is no data for Alumni.<p/>");
+
+    const alumniInfo = []
+
+    function setAlumniInfo(){
+        // set html information to the array
+        alumniInfo.push(<h2 key="Alumni"><b>Alumni</b></h2>);
+        {/* add all information of Alumni */}
+            for (let i = 0; i < alumniData.alumni.length; i++){
+                alumniInfo.push(<p key = {alumniData.alumni[i].name}>Name: <a href={alumniData.alumni[i].url} target="_blank" rel="noopener noreferrer"> {alumniData.alumni[i].name} </a></p>);
+            }
         }
-        courseUI.document.write("</div>");
-      }
-      */
+    
+    setAlumniInfo();
 
     return(
-        <div className="alumni">
-            <h2>Alumni</h2>
-            {alumniData.map((obj) => (
-                // display all information about alumni
-                <p key={obj.name}>{obj.name}: <a href= {obj.url} target="_blank" rel="noopener noreferrer">Link</a></p>
-                    )
-                )
-                }
+        <div className="container">
+            <div className="alumni">
+                {alumniInfo}
+
+                {/* To back Home screen */}
+                <div className="Back">
+                    <Link to={"/"} className="btn btn-outline-secondary">Back</Link>
+                </div>
+            </div>
         </div>
-
-
     )
 }
 
