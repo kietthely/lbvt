6f36f17b37ac6f9853d6e75dc98a693e8fe945d4
SammieDev <lythekiet333@gmail.com>
2024-05-24 22:09:07 +0930

    Test UploadFile
    Allow users to upload files when making a POST request.

--- lbvt-roadmap/src/App.jsx
+++ lbvt-roadmap/src/App.jsx
@@ -1,18 +1,46 @@
+import React, { useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Help from "./components/Help";
+import UploadFile from "./components/UploadFile";
+
 const App = () => {
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [isNextClicked, setIsNextClicked] = useState(false);
+
+  const handleUpload = (data) => {
+    setIsUploaded(true);
+  };
+
+  const handleNextClick = () => {
+    setIsNextClicked(true);
+  };
+
   return (
     <main className="bg-slate-300/20">
       <Router>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Help" element={<Help />} />
-        </Routes>
+        {!isUploaded ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <UploadFile onUpload={handleUpload} onNext={handleNextClick} />
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Help" element={<Help />} />
+          </Routes>
+        )}
       </Router>
     </main>
   );
 };
+
 export default App;
