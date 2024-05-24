069c8fdc4bc17fdcf99ce66a0bf20d0cfcc93eed
SammieDev <lythekiet333@gmail.com>
2024-05-24 22:10:25 +0930

    Add background color to nav menu

--- lbvt-roadmap/src/components/NavBar.jsx
+++ lbvt-roadmap/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <header className="header">
+    <header className="header bg-m-white w-full min-w-full">
       <NavLink
         to="/"
         className="w-12 h-12 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
