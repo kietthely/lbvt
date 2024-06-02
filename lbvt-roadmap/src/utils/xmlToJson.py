e7b75e42a8cc274f7b7217410d785c17fcdd0857
SammieDev <lythekiet333@gmail.com>
2024-06-03 03:46:39 +0930

    Generate iboe json file
    Generate iboe.json via xmlToJson.py and manually update the path to the json file.

--- lbvt-roadmap/src/utils/xmlToJson.py
+++ lbvt-roadmap/src/utils/xmlToJson.py
@@ -3,7 +3,7 @@ import json
 
 def convert_xml_to_json(xml_file):
     # Read xml file
-    with open(xml_file, 'r') as file:
+    with open(xml_file, 'r', encoding='utf-8') as file:
         xml_content = file.read()
     # Parse xml content
     # Some elements have only 1 child but they are supposed to be a list
@@ -11,11 +11,11 @@ def convert_xml_to_json(xml_file):
     json_data = json.dumps(data_dict)
     return json_data
 # Put your xml file path here
-json_data = convert_xml_to_json(r'C:\Users\lythe\Downloads\my_projects\react-practice\lbvt\lbvt-roadmap\src\assets\lbvt.xml')
+json_data = convert_xml_to_json(r'C:\Users\lythe\Downloads\my_projects\react-practice\lbvt\lbvt-roadmap\src\assets\iboe.xml')
 
 def write_json_to_file(json_data, file_path):
     # Generate a json file
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-8') as file:
         file.write(json_data)
 # Put your json file path here 
-write_json_to_file(json_data, r'C:\Users\lythe\Downloads\my_projects\react-practice\lbvt\lbvt-roadmap\src\assets\lbvt.json')
+write_json_to_file(json_data, r'C:\Users\lythe\Downloads\my_projects\react-practice\lbvt\lbvt-roadmap\src\assets\iboe.json')
