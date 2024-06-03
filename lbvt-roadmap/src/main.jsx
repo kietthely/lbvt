8de6e39d1ea87749000487c9eaeee1bee4ce085e
SammieDev <lythekiet333@gmail.com>
2024-06-04 04:51:10 +0930

    Updates

--- lbvt-roadmap/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./output.css";
-import "./index.css";
-import "./styles/_styles.scss";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
